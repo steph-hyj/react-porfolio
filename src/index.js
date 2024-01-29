18a183256d608560615d1f4d0f20a81e1ef264f0
Stephane <ungstephane0@gmail.com>
2024-01-29 04:57:09 +0100

    Basic setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-/*
- * @Author: Stephane ungstephane0@gmail.com
- * @Date: 2024-01-29 04:44:31
- * @LastEditors: Stephane ungstephane0@gmail.com
- * @LastEditTime: 2024-01-29 04:46:15
- * @FilePath: \Projets\react-porfolio\src\index.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
