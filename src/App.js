fa71016b4bf62a28be53d91c02b08d095a2dae14
Stephane <ungstephane0@gmail.com>
2024-01-29 04:47:58 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/*
+ * @Author: Stephane ungstephane0@gmail.com
+ * @Date: 2024-01-29 04:41:52
+ * @LastEditors: Stephane ungstephane0@gmail.com
+ * @LastEditTime: 2024-01-29 04:44:29
+ * @FilePath: \Projets\react-porfolio\src\App.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
