f9e5751d6aece44c6a1f962b093d793f565e2532
Stephane <ungstephane0@gmail.com>
2024-01-30 13:01:08 +0100

    Update navbar component

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -3,20 +3,23 @@ import { Link } from 'react-scroll';
 
 //Import Css file for navbar
 import './navbar.css';
+import { ImBubbles2 } from 'react-icons/im';
 
 const Navbar = () => {
     return (
         <nav className='navbar'>
             <div className='desktopMenu'>
-                <Link className='desktopMenuListItem'>Accueil</Link>
-                <Link className='desktopMenuListItem'>Mes compétences</Link>
-                <Link className='desktopMenuListItem'>Portfolio</Link>
-                <Link className='desktopMenuListItem'>Me contacter</Link>
+                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Accueil</Link>
+                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Mes compétences</Link>
+                <Link activeClass='active' to='works' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Portfolio</Link>
             </div>
             {/* <button className='desktopBtn'>
                 <img src='' alt='' className='desktopImgBtn' />
                 Me contacter
             </button> */}
+            <button className='desktopMenuBtn' onClick={() => { document.getElementById('contact').scrollIntoView({ behavior: 'smooth' }) }}>
+                <ImBubbles2 alt='contact' className='desktopImgBtn' />Me contacter
+            </button>
         </nav>
     )
 }
