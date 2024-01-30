3e90c5e096ceb20cb2190b45818172fab08e5ffa
Stephane <ungstephane0@gmail.com>
2024-01-30 13:01:50 +0100

    Update App.js
    add new pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import Navbar from './components/Navbar/navbar';
 import Intro from './components/Intro/intro';
 import Skills from './components/Skills/skills';
+import Portfolio from './components/Portfolio/portfolio';
+import Contact from './components/Contact/contact';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Navbar />
       <Intro />
       <Skills />
+      <Portfolio />
+      <Contact />
     </div>
   );
 }
